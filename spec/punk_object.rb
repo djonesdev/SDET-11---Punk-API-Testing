8790fd0191417cfe979d1414d3178d185e7788e8
djonesdev <domajones@outlook.com>
2017-10-23 15:50:10 +0100

    have three api calls working

--- spec/punk_object.rb
+++ spec/punk_object.rb
@@ -6,9 +6,25 @@ require 'json'
     include HTTParty
     base_uri 'api.punkapi.com/v2/'
 
-    def single_punk_call(punk)
-      JSON.parse(self.class.get("/postcodes/#{postcode}").body)
+    def random_punk_call
+      ran = JSON.parse(self.class.get("/beers/random").body)
+      ran[0]
     end
 
-    def multiple_punk_service(punk_array)
+    def id_punk_call(id)
+      id = JSON.parse(self.class.get("/beers/#{id}").body)
+      id[0]
     end
+
+
+    def all_punk_service(all)
+        all = JSON.parse(self.class.get("/beers").body)
+        all[0]
+    end
+
+    def abv_punk_call(abv)
+      abv = JSON.parse(self.class.get("/beers/abv_gt").body)
+      abv[0]
+    end
+
+ end
---
+++ spec/punk_test_spec.rb
@@ -0,0 +1,39 @@
+
+
+describe 'Random punk tests' do
+
+  before(:all) do
+    service = PunkService.new
+    @json = service.random_punk_call
+  end
+
+  it 'The id should be a type of integer' do
+    expect(@json["id"]).to be_a_kind_of(Integer)
+  end
+
+end
+
+describe 'Punk by ID tests' do
+
+  before(:all) do
+    service = PunkService.new
+    @jsonid = service.id_punk_call(1)
+  end
+
+  it 'The id should be a type of integer' do
+    expect(@jsonid["id"]).to be_a_kind_of(Integer)
+  end
+
+end
+
+describe 'All punk api' do
+
+  before(:all) do
+    service = PunkService.new
+    @jsonall = service.all_punk_service(1)
+  end
+
+  it 'The id should be a type of integer' do
+    expect(@jsonall["id"]).to be_a_kind_of(Integer)
+  end
+end
